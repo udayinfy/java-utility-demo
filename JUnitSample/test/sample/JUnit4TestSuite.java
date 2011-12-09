2eddde00b0fcc3408067862ed51d61d8fa746376
subversion.hai <subversion.hai@gmail.com>
2011-12-09 03:33:23 +0000

    初始化导入

---
+++ JUnitSample/test/sample/JUnit4TestSuite.java
@@ -0,0 +1,16 @@
+
+package sample;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Sample JUnit-4-style test suite.
+ * 
+ * @author nb
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({sample.VectorsJUnit4Test.class,sample.UtilsJUnit4Test.class})
+public class JUnit4TestSuite {
+
+}
---
+++ JUnitSample/test/sample/MixJUnit3And4TestSuite.java
@@ -0,0 +1,15 @@
+
+package sample;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author nb
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({sample.JUnit4TestSuite.class,sample.VectorsJUnit3Test.class})
+public class MixJUnit3And4TestSuite {
+
+}
